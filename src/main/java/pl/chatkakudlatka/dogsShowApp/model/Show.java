42749aa8b420dd47570efd77b0a11d30a76939a6
Ania Wolke <anna@wolke.pl>
2021-10-09 18:41:24 +0200

    Initial

---
+++ src/main/java/pl/chatkakudlatka/dogsShowApp/model/Show.java
@@ -0,0 +1,10 @@
+package pl.chatkakudlatka.dogsShowApp.model;
+import java.util.Date;
+
+public class Show {
+    private Integer id;
+    private Organizer showOrganizer;
+    private Location showLocation;
+    private Date showDate;
+
+}
